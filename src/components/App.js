a85032b738ad3838dfe9f910e48d4b029a3aa9b7
HectorTorresE <hectorjte@gmail.com>
2022-09-26 23:39:43 -0600

    Add navbar

--- src/components/App.js
+++ src/components/App.js
@@ -8,14 +8,21 @@ import { Provider } from 'react-redux';
 import Cars from './Cars';
 import CarDetails from './CarDetails';
 import store from '../redux/configureStore';
+import Navbar from './Navbar';
+import '../App.css';
 
 const App = () => (
   <Provider store={store}>
     <BrowserRouter>
-      <Routes>
-        <Route path="/cars" element={<Cars />} />
-        <Route path="/car_details" element={<CarDetails />} />
-      </Routes>
+      <div className="bar">
+        <Navbar />
+      </div>
+      <div className="content">
+        <Routes>
+          <Route path="/cars" element={<Cars />} />
+          <Route path="/car_details" element={<CarDetails />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   </Provider>
 );
