a03d18b6b3d94a2c17dca039c78bc43c5bba5c65
jr-cast <castanos@pm.me>
2022-09-28 15:40:26 -0500

    Display HTML elements from localstorage saved state

--- src/components/CarDetails.js
+++ src/components/CarDetails.js
@@ -6,9 +6,63 @@ import { TbSteeringWheel } from 'react-icons/tb';
 const CarDetails = () => {
   const carDetails = useSelector((state) => state.car_details);
   const carDetailsLocal = localStorage.getItem('carDetails');
+  const carDetailsParsed = JSON.parse(carDetailsLocal);
 
   if (carDetails.length === 0) {
-    return <h1>{carDetailsLocal}</h1>;
+    return (
+      <div className="main-details-wrapper w-full h-screen">
+        <div className="details-wrapper">
+          <h1 className="text-3xl font-bold">{carDetailsParsed.brand}</h1>
+          <img className="card-img" src={carDetailsParsed.image} alt={carDetailsParsed.model} />
+          <h4 className="text-3xl font-bold">{carDetailsParsed.model}</h4>
+        </div>
+        <div className="right-side">
+          <table>
+            <tr>
+              <th>Finance fee</th>
+              <td>
+                $
+                {carDetailsParsed.finance_fee}
+              </td>
+            </tr>
+            <tr>
+              <th>Option to purchase fee</th>
+              <td>
+                $
+                {carDetailsParsed.purchase_fee}
+              </td>
+            </tr>
+            <tr>
+              <th>Total ammount payable</th>
+              <td>
+                $
+                {carDetailsParsed.tap}
+              </td>
+            </tr>
+            <tr>
+              <th>Duration</th>
+              <td>
+                {carDetailsParsed.duration}
+                {' months'}
+              </td>
+            </tr>
+            <tr>
+              <th>5.9% APR Representative</th>
+            </tr>
+          </table>
+          <Link className="link-to-cars" to="/cars">
+            DISCOVER MORE MODELS
+          </Link>
+          <button
+            type="submit"
+            className="config-button bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            RESERVE NOW
+            <TbSteeringWheel size={35} />
+          </button>
+        </div>
+      </div>
+    );
   }
   return (
     <div className="main-details-wrapper w-full h-screen">
@@ -21,19 +75,31 @@ const CarDetails = () => {
         <table>
           <tr>
             <th>Finance fee</th>
-            <td>$500</td>
+            <td>
+              $
+              {carDetails.finance_fee}
+            </td>
           </tr>
           <tr>
             <th>Option to purchase fee</th>
-            <td>$500</td>
+            <td>
+              $
+              {carDetails.purchase_fee}
+            </td>
           </tr>
           <tr>
             <th>Total ammount payable</th>
-            <td>$500</td>
+            <td>
+              $
+              {carDetails.tap}
+            </td>
           </tr>
           <tr>
             <th>Duration</th>
-            <td>$500</td>
+            <td>
+              {carDetails.duration}
+              {' months'}
+            </td>
           </tr>
           <tr>
             <th>5.9% APR Representative</th>
