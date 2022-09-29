3fa06f907843f034f10406761a7c779952331cfe
fabianofrank <fabianofrank@protonmail.com>
2022-09-29 15:00:59 -0300

    Update logout component

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -2,27 +2,22 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import { logoutUser } from '../redux/users/users';
+import { logout } from '../redux/users/api';
 
 const Logout = () => {
-  const [userName, setUsername] = useState('');
   const dispatch = useDispatch();
 
-  const userSubmit = (e) => {
-    e.preventDefault();
-    if (userName.length > 3) {
-      dispatch(logoutUser(userName));
-      Navigate('/login');
-    }
-  };
+  useEffect(() => {
+    dispatch(logoutUser());
+  }, [dispatch]);
 
-  return (
-    <div className="signup-page">
-      <form onSubmit={userSubmit}>
-        <input onChange={(e) => setUsername(e.target.value)} className="signInput" type="text" placeholder="username" required />
-        <button type="submit">SignUp</button>
-      </form>
-    </div>
-  );
+  if (logout()) {
+    return (
+      <Navigate to="/" />
+    );
+  }
+
+  return <p>Unable to logout</p>;
 };
 
 export default Logout;
